f2d0aff29d91e4ea4d72058b5d6ad2c80154a7f7
Wei Liu <liuw@liuw.name>
2014-01-14 15:06:37 +0000

    Add Python template

---
+++ template.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+
+def solve():
+    pass
+
+
+def main():
+    import time
+    print "Start"
+    s = time.time()
+    solve()
+    e = time.time()
+    print "Problem solved in", e-s, "second"
+
+
+if __name__ == '__main__':
+    main()
